9182d51edcf1454813c80b8143b8ec2ca8ab0310
tadhg03 <tadhg_mccarthy@dlsu.edu.ph>
2019-12-11 20:15:58 +0800

    fasta and fastq now make a json file

--- fastaParser.py
+++ fastaParser.py
@@ -1,11 +1,23 @@
+import json
+
 try:
+	data = {}
+	data['protein'] =[]
+
 	fp = open("../DATA/mud-crab-virus.fasta")
 	line = fp.readline()
 	cnt = 1
 	while line:
 		if ">" in line:
+			data['protein'].append({
+				'id': line
+			})
 			print(cnt, ": ", line)
+			cnt += 1
 		line = fp.readline()
-		cnt += 1
 finally:
+
+	with open('proteinData.json', 'w') as outfile:
+		json.dump(data, outfile);
+
 	fp.close()
--- fastqParser.py
+++ fastqParser.py
@@ -1,11 +1,23 @@
+import json
+
 try:
+	data = {}
+	data['protein'] =[]
+
 	fp = open("../DATA/sample_1million.fastq")
 	line = fp.readline()
 	cnt = 1
 	while line:
 		if "@" in line:
+			data['protein'].append({
+				'id': line
+			})
 			print(cnt, ": ", line)
 			cnt += 1
 		line = fp.readline()
 finally:
+
+	with open('RNA-Seq-Data.json', 'w') as outfile:
+		json.dump(data, outfile);
+
 	fp.close()
